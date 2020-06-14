da6a43444736f0bce681325bc974ad4e88f5b740
Sören König <soeren.koenig@mailbox.org>
2020-06-14 18:01:50 +0200

    use MetricsCollector's static methods

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -3,16 +3,14 @@ from monitor.collector import MetricsCollector
 
 def test_result_empty():
 
-    collector = MetricsCollector()
-    result = collector.collect("https://httpbin.org/status/400")
+    result = MetricsCollector.collect("https://httpbin.org/status/400")
 
     assert result == {}
 
 
 def test_result():
 
-    collector = MetricsCollector()
-    result = collector.collect("https://httpbin.org/status/200")
+    result = MetricsCollector.collect("https://httpbin.org/status/200")
 
     assert result["status_code"] == 200
     assert isinstance(result["response_time"], int)
